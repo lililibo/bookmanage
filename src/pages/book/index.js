1ead884a0389009fd267d7572aff074718efc6d4
wl1105 <1031270528@qq.com>
2019-05-08 15:37:19 +0800

    把查看按钮组件单独拿出来

--- src/pages/book/index.js
+++ src/pages/book/index.js
@@ -4,6 +4,7 @@ import BookUI from './ui'
 import { inputChange, getBookListAction, searchBookAction, pageClickAction } from './store/createActions';
 import store from '@/store'
 import { Button, Modal } from 'antd'
+import Seenbutton from './Seenbutton'
 import Delbutton from './Delbutton'
 
 
@@ -73,68 +74,6 @@ const mapDispatchToProps = (dispatch) => {
     getBookList: () => {
       dispatch(getBookListAction())
     }
-  }
-}
-
-// 查看按钮
-class Seenbutton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      visible: false,
-      id: '',
-      bookName: '',
-      author: '',
-      price: '',
-      updatedate: ''
-    }
-  }
-  render() {
-    return (
-      <>
-        <Button
-          className='see'
-          style={{ backgroundColor: '#329900', color: '#fff', marginRight: '2px' }}
-          onClick={this.showModal}>
-          查看
-        </Button>
-        <Modal
-          title="图书详情"
-          visible={this.state.visible}
-          onOk={this.handleOk}
-          onCancel={this.handleCancel}
-          footer={null}
-          centered={true}
-        >
-          <p>图书编号:{this.state.id}</p>
-          <p>图书名称:{this.state.bookName}</p>
-          <p>作者：{this.state.author}</p>
-          <p>价格：{this.state.price}</p>
-          <p>上架时间：{this.state.updatedate}</p>
-        </Modal>
-      </>
-    )
-  }
-  showModal = (e) => {
-    var el = e.target.parentNode.parentNode.parentNode;
-    this.setState({
-      visible: true,
-      id: el.children[0].innerText,
-      bookName: el.children[1].innerText,
-      author: el.children[2].innerText,
-      price: el.children[3].innerText,
-      updatedate: el.children[4].innerText
-    });
-  }
-  handleOk = () => {
-    this.setState({
-      visible: false,
-    });
-  }
-  handleCancel = () => {
-    this.setState({
-      visible: false,
-    });
   }
 }
 
